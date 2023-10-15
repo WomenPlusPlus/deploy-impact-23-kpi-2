732d507bded214a9d71ca1d48debea6be18dd19b
Moers <shaaban.moe@gmail.com>
2023-10-15 21:06:13 +0200

    update files

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from enum import Enum, unique
+<<<<<<< HEAD
 from sqlalchemy import func
+=======
+
+>>>>>>> 518ef37 (add models, endpoints)
 
 db = SQLAlchemy()
 
 @unique
 class Periodicity(Enum):
+<<<<<<< HEAD
     yearly = "Yearly"
     quarterly = "Quarterly"
     monthly = "Monthly"
@@ -16,6 +21,18 @@ class Unit(Enum):
     percentage = "%"
     amount = "Amount"
     score = 'Score'
+=======
+    YEARLY = "YEARLY"
+    QUARTERLY = "Quarterly"
+    MONTHLY = "Monthly"
+
+@unique
+class Unit(Enum):
+    UNIT1 = "CHF"
+    UNIT2 = "%"
+    UNIT3 = "Amount" #CHECK WITH ALINA
+    UNIT4 = 'Score'
+>>>>>>> 518ef37 (add models, endpoints)
 
 class User(db.Model):
     """Users Table"""
@@ -29,6 +46,7 @@ class User(db.Model):
     email = db.Column(db.String(250), nullable=False, unique=True)
     user_login_name = db.Column(db.String(250), nullable=False, unique=True)
     active = db.Column(db.Boolean, default=True)
+<<<<<<< HEAD
     is_gatekeeper = db.Column(db.Boolean, default=False)
     
     created_kpi_values = db.relationship('Kpi_Values', backref='created_by', foreign_keys='Kpi_Values.created_by_user_id')
@@ -46,6 +64,12 @@ class User(db.Model):
             'active': self.active,
             'is_gatekeeper': self.is_gatekeeper
         }
+=======
+
+    created_kpi_values = db.relationship('kpi_values', foreign_keys='kpi_values.created_by_user_id', backref='created_by', lazy='dynamic')
+    updated_kpi_values = db.relationship('kpi_values', foreign_keys='kpi_values.updated_by_user_id', backref='updated_by', lazy='dynamic')
+    user_circle = db.relationship('user_circle', foreign_keys = 'user_circle.user_id', backref='user_id')
+>>>>>>> 518ef37 (add models, endpoints)
 
 class Circle(db.Model):
     """Circles Table"""
@@ -55,6 +79,7 @@ class Circle(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(250), unique=True)
 
+<<<<<<< HEAD
  
     circle_user = db.relationship('User_Circle', backref='circle')
 
@@ -64,6 +89,9 @@ class Circle(db.Model):
             'id': self.id,
             'name': self.name,
             }
+=======
+    user_circle = db.relationship('user_circle', foreign_keys='user_circle.circle_id', backref='circle_id')
+>>>>>>> 518ef37 (add models, endpoints)
 
 class Kpi(db.Model):
     """KPI's Table"""
@@ -73,6 +101,7 @@ class Kpi(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     circle_id = db.Column(db.Integer, db.ForeignKey('circles.id'))
     name = db.Column(db.Text, nullable=False, unique=True)
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 <<<<<<< HEAD
@@ -100,6 +129,17 @@ class Kpi(db.Model):
             'target_value':self.target_value,
             'active': self.active
         }
+=======
+    description = db.Column(db.Text, nullable=False)
+    visibility = db.Column(db.Text, nullable=False)
+    periodicity = db.Column(db.Enum(Periodicity), nullable=False)
+    unit = db.Column(db.Enum(Unit), nullable=False)
+    initial_value = db.Column(db.Float) #any default value?
+    target_value = db.Column(db.Float, nullable = False)
+    active = db.Column(db.Boolean, default=True)
+
+    kpi_values = db.relationship('kpi_values', foreign_keys='kpi_values.kpi_id', backref='kpi_id')
+>>>>>>> 518ef37 (add models, endpoints)
 
 class Kpi_Values(db.Model):
     """Kpi Values Table"""
@@ -112,6 +152,7 @@ class Kpi_Values(db.Model):
     period_month = db.Column(db.Integer)
     value = db.Column(db.Float)
     created_by_user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+<<<<<<< HEAD
     created_at = db.Column(db.TIMESTAMP, default=func.now())
     updated_by_user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
     updated_at = db.Column(db.TIMESTAMP, default=func.now(), onupdate=func.now())
@@ -129,11 +170,18 @@ class Kpi_Values(db.Model):
             'updated_at':self.updated_at
         }
 
+=======
+    created_at = db.Column(db.TIMESTAMP)
+    updated_by_user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    updated_at = db.Column(db.TIMESTAMP)
+    
+>>>>>>> 518ef37 (add models, endpoints)
 class User_Circle(db.Model):
     """Users_Circles Table"""
 
     __tablename__ = 'user_circle'
 
+<<<<<<< HEAD
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     circle_id = db.Column(db.Integer, db.ForeignKey('circles.id'), primary_key=True)
 
@@ -173,6 +221,12 @@ class Change_Log(db.Model):
             'registered_at': self.registered_at,
             'activity': self.activity
         }
+=======
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    circle_id = db.Column(db.Integer, db.ForeignKey('circles.id'))
+
+
+>>>>>>> 518ef37 (add models, endpoints)
 
 def connect_db(app):
     db.app = app
